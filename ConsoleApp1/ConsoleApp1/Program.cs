226ee6ae4831f396bd4b55152449319b228cde32
awolferret <104518827+awolferret@users.noreply.github.com>
2022-04-28 22:03:06 +0300

    1

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Сколько перед вами людей?");
+            int people = Convert.ToInt32(Console.ReadLine());
+            int timeForPeople = 10;
+            int waitTime = people * timeForPeople;
+            int minutesInHour = 60;
+            int hours = waitTime / minutesInHour;
+            int minutes = waitTime % minutesInHour;
+            Console.WriteLine($"вам осталось ждать {hours} часов и {minutes} минут");
         }
     }
 }
